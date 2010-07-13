78fa4bdec7b9ace83d68b094ff58fae971419b3b
Alexandre Loureiro Solleiro <alex@webcracy.org>
2010-07-13 14:18:24 +0100

    enabled oauth

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
 ActionController::Routing::Routes.draw do |map|
+
   map.resources :oauth_consumers,:member=>{:callback=>:get}
+  map.resources :users
+  map.resource :session
+
 
   map.logout '/logout', :controller => 'sessions', :action => 'destroy'
   map.login '/login', :controller => 'sessions', :action => 'new'
   map.register '/register', :controller => 'users', :action => 'create'
   map.signup '/signup', :controller => 'users', :action => 'new'
-  map.resources :users
-
-  map.resource :session
+  
+  map.root :controller => 'oauth_consumers'
 
   # The priority is based upon order of creation: first created -> highest priority.
 
