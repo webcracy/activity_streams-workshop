cbf46d344f675ab938c3270b7102e5932e17a9f4
Ruben Fonseca <root@cpan.org>
2010-07-14 15:21:02 +0100

    protected activities from anonymous reading

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,13 +1,16 @@
 class ActivitiesController < ApplicationController
-  
   before_filter :login_or_oauth_required, :except => [:index, :new, :create, :show_from_permalink]
   
   def index
-    @activities = Activity.find(:all)
+    if logged_in?
+      @activities = current_user.activities
+    else
+      @activities = (session[:activities] || []).map { |a| Activity.find(a) rescue nil }.compact
+    end
   end
 
   def show
-    @activity = Activity.find(params[:id])
+    @activity = find_from_session_or_user(params[:id])
   end
   
   def show_from_permalink
@@ -37,11 +40,11 @@ class ActivitiesController < ApplicationController
   end
   
   def edit
-    @activity = Activity.find(params[:id])
+    @activity = find_from_session_or_user(params[:id])
   end
   
   def update
-    @activity = Activity.find(params[:id])
+    @activity = find_from_session_or_user(params[:id])
     @activity.user = current_user
 
     if @activity.update_attributes(params[:activity])
@@ -53,9 +56,26 @@ class ActivitiesController < ApplicationController
   end
   
   def destroy
-    @activity = Activity.find(params[:id])
+    @activity = find_from_session_or_user(params[:id])
     @activity.destroy
     flash[:notice] = "Successfully destroyed activity."
     redirect_to activities_url
+  end
+
+  private
+  def find_from_session_or_user(id)
+    # if the user is logged in, find from owned activities...
+    if logged_in?
+      current_user.activities.find(id)
+
+    # ... otherwhise, search for the activity id on the session
+    else
+      if session[:activities].find(id.to_i).present?
+        Activity.find(params[:id])
+      else
+        # everything fails, so give back 404
+        raise ActiveRecord::RecordNotfound
+      end
+    end
   end
 end
