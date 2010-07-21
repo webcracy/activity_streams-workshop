242bbc6d755734b8f2b46253c4f2c372206bc382
Alexandre Loureiro Solleiro <alex@webcracy.org>
2010-07-21 18:09:09 +0100

    created home controller and first drafts of home and faq pages

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,10 @@ ActionController::Routing::Routes.draw do |map|
   map.resources :users
   map.resource :session
   map.permalink '/a/:id', :controller => 'activities', :action => 'show_from_permalink'
+  map.faq '/faq', :controller => 'home', :action => 'faq'
+  map.about '/about', :controller => 'home', :action => 'faq'
   map.logout '/logout', :controller => 'sessions', :action => 'destroy'
   map.login '/login', :controller => 'OauthConsumers', :action => 'index'
   
-  map.root :controller => 'activities', :action => "new"
+  map.root :controller => 'home', :action => 'index'
 end
