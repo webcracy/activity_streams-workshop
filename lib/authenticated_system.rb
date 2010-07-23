e7f7df9b307ebc4ea20124e476d1bc13f292cab7
Ruben Fonseca <root@cpan.org>
2010-07-23 14:04:28 +0100

    fixed problem with remember_token

--- lib/authenticated_system.rb
+++ lib/authenticated_system.rb
@@ -122,7 +122,10 @@ module AuthenticatedSystem
     # Called from #current_user.  Finaly, attempt to login by an expiring token in the cookie.
     # for the paranoid: we _should_ be storing user_token = hash(cookie_token, request IP)
     def login_from_cookie
-      user = !cookies[:auth_token].blank? and User.find_by_remember_token(cookies[:auth_token])
+      if cookies[:auth_token].present?
+        user = User.find_by_remember_token(cookies[:auth_token])
+      end
+
       if user && user.remember_token?
         self.current_user = user
         handle_remember_cookie! false # freshen cookie token (keeping date)
